cfa2bff0be23c1a39d32aa7860e641d07253370f
Andrey Osipov <mitol5549@gmail.com>
2023-10-10 13:27:54 +0200

    landing page added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,9 @@
-import { cn } from '@/lib/utils';
-import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+
+import { cn } from '@/lib/utils';
+
+import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
 
